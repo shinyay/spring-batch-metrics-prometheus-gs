1b8022a342edd2b64ad7ff25c68374301b792d6d
shinyay <shinya.com@gmail.com>
2021-03-20 14:03:54 +0900

    Add simple tasklet to display CurrentTime

--- src/main/kotlin/com/google/shinyay/config/BatchConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/BatchConfig.kt
@@ -1,9 +1,25 @@
 package com.google.shinyay.config
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.step.tasklet.TaskletStep
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableBatchProcessing
-class BatchConfig {
+class BatchConfig(val stepBuilderFactory: StepBuilderFactory, val jobBuilderFactory: JobBuilderFactory) {
+
+    @Bean
+    fun helloTasklet(): HelloTasklet {
+        return HelloTasklet()
+    }
+
+    @Bean
+    fun helloStep(): TaskletStep {
+        return stepBuilderFactory.get("HelloStep")
+            .tasklet(helloTasklet())
+            .build()
+    }
 }
